f6a6927caef6fc94d17f48b0ee49b027f8112fff
ryan-williams <nobigdealstyle@gmail.com>
2023-10-31 15:36:54 +0000

    Deploying to ghp from @ ryan-williams/next-duckdb-parquet-demo@fad9e551cc2d0ace0b6171448589c6efab0e76e1 🚀

---
+++ _next/static/9x_YcjPcrpMuU7kulykYq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/504-cf5ab14f9d9a006c.js","static/chunks/pages/index-8f8f38a215d555bd.js"],"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/I-9Ilf0O4YLtXWV1HAj_i/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/504-cf5ab14f9d9a006c.js","static/chunks/pages/index-cef68089d0ddb7bf.js"],"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
